cc34375a15481f0c1086c20d240544a3c323b787
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-07-12 17:00:11 +0530

    completed project

--- client/src/components/layout/Sidebar.jsx
+++ client/src/components/layout/Sidebar.jsx
@@ -1,7 +1,22 @@
 import { NavLink } from "react-router-dom";
 import { sidebarMenu } from "../../utils/sidebarMenu.js";
-
+import { useNavigate } from "react-router-dom";
+import { logoutUser } from "../../services/authService";
 const Sidebar = () => {
+
+
+
+  const navigate = useNavigate();
+
+
+  const handleLogout = () => {
+
+    logoutUser();
+
+    navigate("/login");
+
+  };
+
   return (
     <aside className="w-64 min-h-screen bg-white border-r border-gray-200 shadow-sm">
 
@@ -24,10 +39,9 @@ const Sidebar = () => {
               to={item.path}
               className={({ isActive }) =>
                 `flex items-center gap-3 px-4 py-3 rounded-xl transition-all duration-200
-                ${
-                  isActive
-                    ? "bg-emerald-500 text-white shadow"
-                    : "text-gray-700 hover:bg-emerald-50 hover:text-emerald-600"
+                ${isActive
+                  ? "bg-emerald-500 text-white shadow"
+                  : "text-gray-700 hover:bg-emerald-50 hover:text-emerald-600"
                 }`
               }
             >
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,7 +1,43 @@
 import api from "./api";
 
-export const loginUser = async (formData) => {
-  const response = await api.post("/auth/login", formData);
 
-  return response.data;
+export const loginUser = async (formData) => {
+
+    const response = await api.post(
+        "/auth/login",
+        formData
+    );
+
+    return response.data;
+
+};
+
+
+
+
+
+export const logoutUser = () => {
+
+
+    // Remove JWT token
+
+    localStorage.removeItem("token");
+
+
+    // Remove user details if stored
+
+    localStorage.removeItem("user");
+
+
+
+    // Remove axios authorization header
+
+    delete api.defaults.headers.common[
+        "Authorization"
+    ];
+
+
+
+    return true;
+
 };
