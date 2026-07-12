4b3ee6cc592da97af23b89b19712a21d4cacde90
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-07-12 09:02:07 +0530

    mongoDB connected

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+
+    res.send("AssetFlow API Running");
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+    console.log(`Server Running on Port ${PORT}`);
+
+});
