f092522ee05527a1a8d52f30ac5f5eec49ad96f4
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-07-12 11:05:07 +0530

    completed employee register api

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
+import protect from "../middleware/authMiddleware.js";
+import authorize from "../middleware/roleMiddleware.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/login", login);
+
+// only asset manager can register users
+authRoutes.post("/register", protect, authorize("Asset Manager"), register);
 
 export default authRoutes;
 
